915d97e14a982628fdfa5db366ed5fb9b1bb5c9a
LQHsu <zzruzlan@gmail.com>
2023-05-24 23:09:49 -0600

    Mas errores

--- src/main/java/mx/uam/ayd/proyecto/negocio/ServicioVenta.java
+++ src/main/java/mx/uam/ayd/proyecto/negocio/ServicioVenta.java
@@ -29,7 +29,7 @@ public class ServicioVenta {
         List<Producto> productos = new ArrayList<>();
 
         for (Venta venta : ventaRepository.findByFecha(fecha)) {
-            List<Producto> productosVenta = venta.getProductos();
+            List<Producto> productosVenta = venta.getListaProductos();
             for (Producto producto : productosVenta) {
                 productos.add(producto);
             }
@@ -53,6 +53,48 @@ public class ServicioVenta {
         return importeTotal;
     }
 	
-	
+	public Venta CrearVenta()
+{
+    Venta venta = new Venta();
+    LocalDate fecha = LocalDate.now();
+    venta.setTotal(0);
+    venta.setFecha(fecha);
+    return venta;
+}
+
+public boolean agregarProducto(Producto producto,Venta venta)
+{ 
+    
+    if(producto.getStock() == 0)
+    {
+        throw new IllegalArgumentException("No hay mas inventario de ese producto");
+    }else{
+        venta.setTotal(venta.getTotal()+ producto.getPrecio());
+        int stock = producto.getStock();
+        producto.setStock(stock-1);
+        productoRepository.save(producto);
+        return  venta.agregarProducto(producto);}
+    }
+
+    public boolean quitarProducto(Producto producto, int lugar,Venta venta)
+{
+    venta.setTotal(venta.getTotal() - producto.getPrecio());
+
+    producto.setStock(producto.getStock()+1);
+    productoRepository.save(producto);
+
+    return venta.quitarProducto(lugar);
+}
+
+public int finalizarVenta(int PagoCliente,Venta venta)
+{
+    int cambio = 0;
+    if(venta.getTotal()>PagoCliente)
+    { throw new IllegalArgumentException("El pago del cliente debe cublir el total de la venta");}
+    else{cambio = (int) (PagoCliente - venta.getTotal());
+        ventaRepository.save(venta);
+        if(cambio < 0){cambio = 0;}
+         return cambio;}
+        }
 
 }
